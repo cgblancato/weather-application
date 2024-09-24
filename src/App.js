58c10b22a3a28299973eeae0e85bbd970cdf2f3e
DESKTOP-ILNL1KN\nadakaineko <c.g.blancato@gmail.com>
2024-09-24 15:01:23 -0400

    Updating default state to render blank until user input. Basic styling next.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Temperature from "./Components/Temperature"
 function App() {
   const [weatherData, setWeatherData] = useState(null)
   const [error, setError] = useState(null)
-  const [location, setLocation] = useState("New York") // default values set to NYC
-  const [apiLocationName, setapiLocationName] = useState("New York")
-  const [latitude, setLatitude] = useState(40.710335) // default values set to NYC
-  const [longitude, setLongitude] = useState(-73.99309) // default values set to NYC
+  const [location, setLocation] = useState(null)
+  const [apiLocationName, setapiLocationName] = useState(null)
+  const [latitude, setLatitude] = useState(null)
+  const [longitude, setLongitude] = useState(null)
   const [temperature, setTemperature] = useState(null)
   const inputLocation = useRef()
 
@@ -22,32 +22,32 @@ function App() {
   useEffect(() => {
     const options = { method: 'GET', headers: { accept: 'application/json' } }
 
-    const getWeather = () => {
-      fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&temperature_unit=fahrenheit`, {method: 'GET', headers: { accept: 'application/json' }})
+    if (location) {
+      const getWeather = () => {
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&temperature_unit=fahrenheit`, {method: 'GET', headers: { accept: 'application/json' }})
+          .then(response => response.json())
+          .then(data => {
+            setWeatherData(data)
+            setTemperature(Math.floor(data.current.temperature_2m))
+          })
+          .catch(err => {
+            setError("No location found. Please check your input and try again.")
+          })
+      }
+  
+      fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=10&language=en&format=json`, options)
         .then(response => response.json())
         .then(data => {
-          setWeatherData(data)
-          setTemperature(Math.floor(data.current.temperature_2m))
+          setapiLocationName(data.results[0].name)
+          setLatitude(data.results[0].latitude)
+          setLongitude(data.results[0].longitude)
+        }).then(() => {
+          getWeather()
         })
         .catch(err => {
           setError("No location found. Please check your input and try again.")
         })
     }
-
-    fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=10&language=en&format=json`, options)
-      .then(response => response.json())
-      .then(data => {
-        setapiLocationName(data.results[0].name)
-        setLatitude(data.results[0].latitude)
-        setLongitude(data.results[0].longitude)
-      }).then(() => {
-        getWeather()
-      })
-      .catch(err => {
-        setError("No location found. Please check your input and try again.")
-      })
-
-    
   }, [location, latitude, longitude])
 
   return (
