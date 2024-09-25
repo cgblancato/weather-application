8d5ca774ff1301b9e3d58db660862c7bb0777d89
DESKTOP-ILNL1KN\nadakaineko <c.g.blancato@gmail.com>
2024-09-24 20:35:22 -0400

    Adding location state and updating placeholder text.

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
       fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=10&language=en&format=json`, {method: 'GET', headers: { accept: 'application/json' }})
         .then(response => response.json())
         .then(data => {
-          setapiLocationName(data.results[0].name)
+          setapiLocationName(data.results[0].name + ", " + data.results[0].admin1)
           setLatitude(data.results[0].latitude)
           setLongitude(data.results[0].longitude)
         }).then(() => {
@@ -69,7 +69,7 @@ function App() {
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                     </svg>
                 </div>
-                <input type="search" id="location-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for the name of your city or state..." ref={inputLocation} />
+                <input type="search" id="location-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for the ZIP code or city name..." ref={inputLocation} />
                 <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
               </div>
               {error && (
